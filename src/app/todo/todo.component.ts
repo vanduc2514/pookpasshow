107484e3c85b9fe437131d582082ec899ec12224
Duc <vanduc2514@gmail.com>
2020-07-09 10:36:54 +0700

    Add Logic for Todo Component Add Port for App

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ITodo} from '../itodo';
+import {FormControl} from '@angular/forms';
+import {TodoService} from '../todo.service';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
+  todoList: ITodo[] = [];
+  inputControl = new FormControl('');
 
-  constructor() { }
+  constructor(private todoService: TodoService) {
+  }
 
   ngOnInit(): void {
+  }
+
+  toggleTodo(i): void {
+  }
+
+  addTodo(): void {
+  }
+
+  deleteTodo(): void {
   }
 
 }
